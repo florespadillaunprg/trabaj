9fec56982470ddb47da4c74bd603de385a6283f5
florespadillaunprg <57303134+florespadillaunprg@users.noreply.github.com>
2019-12-11 21:56:47 -0500

    se agrego el programa indexacion_olazabal

---
+++ olazabal/indexacion.py
@@ -0,0 +1,89 @@
+# Ejercicio 01
+#                 10        20        30        40        50        60        70        80        90       100
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901
+frase01="la vida es una obra teatral que no importa cuanto haya durado, sino lo bien que haya sido representada"
+print(frase01[1],frase01[26],frase01[33],frase01[97],frase01[55],frase01[39],frase01[53])   #Devuelve "alondra"
+
+# Ejercicio 02
+#                 10        20        30        40        50        60        70        80        90       100       110
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+frase02="no cambies lo que mas quieres en la vida por lo que mas quieres en el momento, porque los momentos pasan y la vida sigue"
+print(frase02[39], frase02[8], frase02[37], frase02[80], frase02[118])       #Devuelve las vocales
+
+# Ejercicio 03
+#                 10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890123
+frase03="cuando no sepas donde ir, sigue el perfume de un sueno"
+print(frase03[35], frase03[11], frase03[23], frase03[37], frase03[53])          #Devuelve "perro"
+
+# Ejercicio 04
+#                 10        20        30        40        50        60        70        80        90       100       110       120
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012
+frase04="es una locura odiar a todas las rosas porque una te pincho o renunciar a todos tus suenos porque uno de ellos no se realizo"
+print(frase04[5], frase04[75], frase04[0], frase04[119], frase04[68], frase04[46], frase04[79], frase04[102])   # Devuelve "adelante"
+
+# Ejercicio 05
+#                 10        20        30        40        50        60        70        80        90       100
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345678901234
+frase05="todo el mundo trata de realizar algo grande, sin darse cuenta de que la vida se compone de cosas pequenas"
+print(frase05[72], frase05[27], frase05[55], frase05[14], frase05[84], frase05[15], frase05[46], frase05[94])     #Devuelve "victoria"
+
+# Ejercicio 06
+#                 10        20        30        40
+#        0123456789012345678901234567890123456789012345
+frase06="no hay pasion mas ilusa y fanatica que el odio"
+print(frase06[19], frase06[45], frase06[32], frase06[1])        #Devuelve "loco"
+
+# Ejercicio 07
+#                 10        20        30        40        50        60        70        80
+#        01234567890123456789012345678901234567890123456789012345678901234567890123456789012345
+frase07="nuestra mayor gloria no esta en no caer nunca, sino en levantarnos cada vez que caemos"
+print(frase07[3],frase07[58],frase07[55],frase07[1],frase07[69])     #Devuelve "salud"
+
+# Ejercicio 08
+#                 10        20        30        40        50        60        70
+#        0123456789012345678901234567890123456789012345678901234567890123456789012345678
+frase08="se necesitan dos anios para aprender a hablar, y sesenta para aprender a callar"
+print(frase08[1],frase08[51],frase08[63],frase08[34],frase08[5],frase08[19],frase08[71],frase08[42])    #Devuelve "especial"
+
+# Ejercicio 09
+#                 10        20        30        40        50        60        70        80
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890123
+frase09="nunca sabes lo fuerte que eres, hasta que ser fuerte es la unica opcion que te queda"
+print(frase09[66],frase09[7],frase09[29],frase09[61],frase09[69], frase09[0])        #Devuelve "pasion"
+
+# Ejercicio 10
+#                 10        20        30        40        50        60        70        80        90
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345
+frase10="la vida no se juzga por el tiempo, sino por los recuerdos de los momentos especiales que vivimos"
+print(frase10[1],frase10[61],frase10[29],frase10[17],frase10[53],frase10[90],frase10[18])      # Devuelve "alegria"
+
+# Ejercicio 11
+#                 10        20        30        40        50
+#        012345678901234567890123456789012345678901234567890123
+frase11="cada hombre puede mejorar su vida mejorando su actitud"
+print(frase11[36],frase11[52],frase11[29],frase11[14],frase11[40],frase11[49],frase11[13],frase11[41])     #Devuelve "juventud"
+
+# Ejercicio 12
+#                 10        20        30        40        50        60        70        80
+#        012345678901234567890123456789012345678901234567890123456789012345678901234567890
+frase12="el exito no se mide en el dinero, sino en la diferencia que marca en las personas"
+print(frase12[63],frase12[69],frase12[79],frase12[12],frase12[0])        #Devuelve "clase"
+
+# Ejercicio 13
+#                 10        20        30        40        50
+#        0123456789012345678901234567890123456789012345678901
+frase13="no hay camino para la verdad, la verdad es el camino"
+print(frase13[7],frase13[37],frase13[19],frase13[1],frase13[24])       #Devuelve "calor"
+
+# Ejercicio 14
+#                 10        20        30        40        50        60        70        80        90       100
+#        0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789012345
+frase14="hay dos maneras de vivir la vida: una como si nada fuese un milagro, la otra como si todo fuese un milagro"
+print(frase14[11],frase14[54],frase14[85],frase14[29],frase14[99],frase14[49])      #Devuelve "estima"
+
+# Ejercicio 15
+#                 10        20        30        40        50        60        70        80        90
+#        0123456789012345678901234567890123456789012345678901234567890123456789012345678901234567890123456789
+frase15="feliz es el hombre que aprende temprano el amplio abismo existente entre sus deseos y sus facultades"
+print(frase15[92],frase15[59],frase15[93],frase15[77],frase15[23],frase15[97])   #Devuelve "ciudad"
